24b64fdebed74dc228e3e1b64ce9d3900a772301
joaonative <jestudos127@gmail.com>
2023-12-06 00:49:31 -0300

    feat: hero seciton component

---
+++ src/components/Landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <section className="flex flex-col gap-[75px]">
+      <div className="w-full px-32 mt-20">
+        <div className="flex items-center justify-between gap-5">
+          <blockquote className="font-extrabold title">
+            <h1>ESCOLHA ENTRE NOSSAS</h1>
+            <h1>SOLUÇÕES NATURAIS DE </h1>
+            <h1 className="text-primary">SKIN CARE.</h1>
+          </blockquote>
+          <div className="flex items-center justify-center bg-primary rounded-full w-[450px] h-[450px] pt-5 sombra">
+            <img src="/landing.png" alt="" className="" />
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[440px] bg-cover bg-landing"></div>
+    </section>
+  );
+};
+
+export default Landing;
