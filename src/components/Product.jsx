d0ca68827b510448501979edb5c6873aa34f1ea5
joao native <jestudos127@gmail.com>
2023-12-07 17:32:04 -0300

    feat: productcard component

---
+++ src/components/Product.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Product = ({ name, price, imageUrl, brand, id }) => {
+  return (
+    <div className="shadow-xl bg-white overflow-hidden flex flex-col items-center justify-start text-xl font-medium rounded-xl gap-4 pb-3">
+      <div>
+        <Image
+          src={imageUrl}
+          width={300}
+          height={200}
+          alt={`produto cosmético ${name} por ${brand}`}
+        />
+      </div>
+      <h1>
+        {name} - <span>{brand}</span>
+      </h1>
+      <span> R$ {price}</span>
+      <Link href={`/produtos/${id}`}>
+        <button className="primary">VER MAIS</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
